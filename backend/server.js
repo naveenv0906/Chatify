b53ae8157c3c736d9f6b99028e9fc430adc37656
naveenv0906 <naveenv0906@gmail.com>
2024-09-02 12:02:58 +0530

    404 change

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use("/api/users", userRoutes);
 app.use(express.static(path.join(__dirname, "frontend", "dist")));
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+	res.send("404 page not found");
 });
 
 // Global error handler
